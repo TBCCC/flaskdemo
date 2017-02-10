3c16270994e642c83a205a6f0a5e194bafa9aece
TBCCC <tbccc55555@gmail.com>
2017-02-10 11:21:47 +0800

    add the first test

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -6,16 +6,18 @@ import tempfile
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd,flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING']=True
-        self.app = falskr.app.test_client()
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
         flaskr.init_db()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unl
         os.unlink(flaskr.app.config["DATABASE"])
 
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
